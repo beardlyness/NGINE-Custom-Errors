6f92d8ec32b2730ea88eae4782c5839745df95af
Beard <beardlyness@users.noreply.github.com>
2019-04-28 08:02:51 -0700

    Update 404.php

--- errors/php/404.php
+++ errors/php/404.php
@@ -9,7 +9,7 @@
         <p>The origin server did not find a current representation for the target resource or is not willing to disclose that one exists.</p>
         <p>Server: <code><?php echo $server_ip; ?></code> | Visitor: <code><?php echo $origin_ip; ?></code></p>
         <p>Hostname: <code><?php echo $server_host; ?></code></p>
-        <o>URI Request: <code><?php echo $url_path; ?></code></p>
+        <p>URI Request: <code><?php echo $url_path; ?></code></p>
         <p>Web Directory: <code>/var/www/html/domain/live</code></p>
     </section>
 
