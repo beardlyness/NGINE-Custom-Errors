fb40f2590c47937d8ff851edb8ca38750fc19b10
Beard <beardlyness@users.noreply.github.com>
2019-04-28 08:02:42 -0700

    Update 403.php

--- errors/php/403.php
+++ errors/php/403.php
@@ -9,7 +9,7 @@
         <p>The server understood the request but refuses to authorize it.</p>
         <p>Server: <code><?php echo $server_ip; ?></code> | Visitor: <code><?php echo $origin_ip; ?></code></p>
         <p>Hostname: <code><?php echo $server_host; ?></code></p>
-        <o>URI Request: <code><?php echo $url_path; ?></code></p>
+        <p>URI Request: <code><?php echo $url_path; ?></code></p>
         <p>Web Directory: <code>/var/www/html/domain/live</code></p>
     </section>
 
