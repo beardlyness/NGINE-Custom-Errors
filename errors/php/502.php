0b9b00de1907691cee53f645b8bc86ca12e978e5
Beard <beardlyness@users.noreply.github.com>
2019-04-28 08:04:34 -0700

    Update 502.php

--- errors/php/502.php
+++ errors/php/502.php
@@ -9,7 +9,7 @@
         <p>The server, while acting as a gateway or proxy, received an invalid response from an inbound server it accessed while attempting to fulfill the request.</p>
         <p>Server: <code><?php echo $server_ip; ?></code> | Visitor: <code><?php echo $origin_ip; ?></code></p>
         <p>Hostname: <code><?php echo $server_host; ?></code></p>
-        <o>URI Request: <code><?php echo $url_path; ?></code></p>
+        <p>URI Request: <code><?php echo $url_path; ?></code></p>
         <p>Web Directory: <code>/var/www/html/domain/live</code></p>
     </section>
 
