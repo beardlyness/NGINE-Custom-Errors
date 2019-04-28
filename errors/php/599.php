40f77d198dca52ae4bf1737715351d6a47398c65
Beard <beardlyness@users.noreply.github.com>
2019-04-28 08:05:26 -0700

    Update 599.php

--- errors/php/599.php
+++ errors/php/599.php
@@ -9,7 +9,7 @@
         <p>This status code is not specified in any RFCs, but is used by some HTTP proxies to signal a network connect timeout behind the proxy to a client in front of the proxy.</p>
         <p>Server: <code><?php echo $server_ip; ?></code> | Visitor: <code><?php echo $origin_ip; ?></code></p>
         <p>Hostname: <code><?php echo $server_host; ?></code></p>
-        <o>URI Request: <code><?php echo $url_path; ?></code></p>
+        <p>URI Request: <code><?php echo $url_path; ?></code></p>
         <p>Web Directory: <code>/var/www/html/domain/live</code></p>
     </section>
 
