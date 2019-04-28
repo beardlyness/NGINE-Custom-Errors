b24b16825cad2f6abbd16ae49f85ed2afdcb1437
Beard <beardlyness@users.noreply.github.com>
2019-04-28 08:03:19 -0700

    Update 407.php

--- errors/php/407.php
+++ errors/php/407.php
@@ -9,7 +9,7 @@
         <p>The proxy MUST send a Proxy-Authenticate header field containing a challenge applicable to that proxy for the target resource. The client MAY repeat the request with a new or replaced Proxy-Authorization header field.</p>
         <p>Server: <code><?php echo $server_ip; ?></code> | Visitor: <code><?php echo $origin_ip; ?></code></p>
         <p>Hostname: <code><?php echo $server_host; ?></code></p>
-        <o>URI Request: <code><?php echo $url_path; ?></code></p>
+        <p>URI Request: <code><?php echo $url_path; ?></code></p>
         <p>Web Directory: <code>/var/www/html/domain/live</code></p>
     </section>
 
