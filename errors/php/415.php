be61f058cdde1c12ad0fbca423b59a6d7a90fded
Beard <beardlyness@users.noreply.github.com>
2019-04-28 08:03:54 -0700

    Update 415.php

--- errors/php/415.php
+++ errors/php/415.php
@@ -9,7 +9,7 @@
         <p>The origin server is refusing to service the request because the payload is in a format not supported by this method on the target resource.</p>
         <p>Server: <code><?php echo $server_ip; ?></code> | Visitor: <code><?php echo $origin_ip; ?></code></p>
         <p>Hostname: <code><?php echo $server_host; ?></code></p>
-        <o>URI Request: <code><?php echo $url_path; ?></code></p>
+        <p>URI Request: <code><?php echo $url_path; ?></code></p>
         <p>Web Directory: <code>/var/www/html/domain/live</code></p>
     </section>
 
